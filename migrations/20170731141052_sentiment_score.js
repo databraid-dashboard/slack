934df050f77d514aff68260abe272b38d976447b
Meghan Prestemon <m.m.hares@gmail.com>
2017-07-31 16:45:43 -0700

    Create seeds for sentiment_score and build out express

--- migrations/20170731141052_sentiment_score.js
+++ migrations/20170731141052_sentiment_score.js
@@ -3,9 +3,9 @@
 exports.up = function(knex) {
   return knex.schema.createTable('sentiment_score', (table) => {
     table.increments('id').primary();
-    table.integer('channel_map_id').notNullable().references('id').inTable('channel_map').onDelete('CASCADE').index();
-    table.integer('score').notNullable();
-    table.integer('magnitude').notNullable();
+    //table.integer('channel_map_id').notNullable().references('id').inTable('channel_map').onDelete('CASCADE').index();
+    table.decimal('score', 3, 2).notNullable();
+    table.float('magnitude').notNullable();
     table.integer('number_of_messages').notNullable();
     table.timestamps(true, true);
   })
---
+++ seeds/4_sentiment_score.js
@@ -0,0 +1,32 @@
+
+exports.seed = function(knex) {
+  return knex('sentiment_score').del()
+    .then(function () {
+      return knex('sentiment_score').insert([
+        {
+          'id': 1,
+          'score': 0.50,
+          'magnitude': 30,
+          'number_of_messages': 100,
+          'created_at': new Date('2017-07-31 14:26:16 UTC'),
+          'updated_at': new Date('2017-07-31 14:26:16 UTC')
+        },
+        {
+          'id': 2,
+          'score': -0.50,
+          'magnitude': 32,
+          'number_of_messages': 100,
+          'created_at': new Date('2017-07-31 14:26:16 UTC'),
+          'updated_at': new Date('2017-07-31 14:26:16 UTC')
+        },
+        {
+          'id': 3,
+          'score': 0.02,
+          'magnitude': 15,
+          'number_of_messages': 100,
+          'created_at': new Date('2017-07-31 14:26:16 UTC'),
+          'updated_at': new Date('2017-07-31 14:26:16 UTC')
+        }
+      ]);
+    });
+};
