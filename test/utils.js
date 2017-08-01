549474abab8b7f6dd9345eed5bc17ce5fba1989e
Meghan Prestemon <m.m.hares@gmail.com>
2017-08-01 13:34:36 -0700

    Complete lint compliance configuration and modify app.js for production

--- test/utils.js
+++ test/utils.js
@@ -2,7 +2,8 @@ const knex = require('../knex');
 
 // add knex database hooks to a test suite to tear down and build back
 // up the database on each test in test suite.
-const addDatabaseHooks = func => function (...args) {
+const addDatabaseHooks = func => (...args) => {
+  /* eslint-disable no-undef */
   beforeEach((done) => {
     knex.migrate.rollback()
       .then(() => knex.migrate.latest())
@@ -12,6 +13,7 @@ const addDatabaseHooks = func => function (...args) {
       });
   });
 
+  /* eslint-disable no-undef */
   afterEach((done) => {
     knex.migrate.rollback()
       .finally(() => {
