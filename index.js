656e24c2664a80fe2472752dc436db2a47c470c5
Meghan Prestemon <m.m.hares@gmail.com>
2017-08-02 11:38:07 -0700

    Remove app.js and integrate into index.js

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
+
 const express = require('express');
 const path = require('path');
-
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 
 const app = express();
 
+const index = require('./routes/score');
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/score', score);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
@@ -25,6 +43,15 @@ app.use((err, req, res) => {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  if (app.get('env') !== 'test') {
+    /* eslint-disable no-console */
+    console.log('Listening on port', port);
+  }
 });
 
 module.exports = app;
