54fdba7eec5958a93cd15462e104eb304bc53db6
Dave Gallup <Dave-Gallup@users.noreply.github.com>
2017-08-03 11:50:11 -0700

    Slack app (#2)
    * Add listen to index.js
    
    * Connect Slack App to server & verify event subscription via console
    
    * Tweak routes and alter html button location

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
+require('dotenv').config();
 const express = require('express');
 const path = require('path');
-
 const bodyParser = require('body-parser');
+const slack = require('./routes/slack');
+const index = require('./routes/index');
 
 const app = express();
+const PORT = process.env.PORT || 8000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/slack', slack);
+app.use('/', index);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
@@ -25,6 +31,11 @@ app.use((err, req, res) => {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
+});
+
+app.listen(PORT, () => {
+  /* eslint-disable no-console */
+  console.log(`Express server listening on port ${PORT}`);
 });
 
 module.exports = app;
