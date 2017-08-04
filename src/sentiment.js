28b61c731635d6d049a8d3c58ac5f450c3713df3
Meghan Prestemon <m.m.hares@gmail.com>
2017-08-04 13:20:03 -0700

    Update pull request suggestions

--- src/sentiment.js
+++ src/sentiment.js
@@ -26,7 +26,7 @@ function analyzeSentimentOfText(messageString, channelId, numberOfMessages) {
         numberOfMessages,
       );
     })
-    .then(scoreData => console.log(scoreData[0].score))
+    .then(scoreData => scoreData[0].score)
     .catch(err => err);
 }
 
@@ -40,7 +40,5 @@ function analyzeSentimentAndSaveScore(channelId) {
     })
     .catch(err => err);
 }
-
-analyzeSentimentAndSaveScore(1);
 
 module.exports = { analyzeSentimentAndSaveScore, analyzeSentimentOfText };
--- test/score.test.js
+++ test/score.test.js
@@ -1,20 +1,16 @@
+/* eslint-disable no-undef, no-unused-expressions */
 const expect = require('chai').expect;
 const { analyzeSentimentOfText, analyzeSentimentAndSaveScore } = require('../src/sentiment.js');
 
-/* eslint-disable no-undef */
 describe('analyzeSentimentOfText', () => {
-  /* eslint-disable no-undef */
   it('should exist', () => {
-    /* eslint-disable no-unused-expressions */
     expect(analyzeSentimentOfText).to.exist;
   });
 
-  /* eslint-disable no-undef */
   it('should be a function', () => {
     expect(analyzeSentimentOfText).is.a('function');
   });
 
-  /* eslint-disable no-undef */
   it('should return a number', () => {
     analyzeSentimentOfText('This is a string of text.', 1, 3)
       .then((result) => {
@@ -23,15 +19,11 @@ describe('analyzeSentimentOfText', () => {
   });
 });
 
-/* eslint-disable no-undef */
 describe('analyzeSentimentAndSaveScore', () => {
-  /* eslint-disable no-undef */
   it('should exist', () => {
-    /* eslint-disable no-unused-expressions */
     expect(analyzeSentimentAndSaveScore).to.exist;
   });
 
-  /* eslint-disable no-undef */
   it('should be a function', () => {
     expect(analyzeSentimentAndSaveScore).is.a('function');
   });
