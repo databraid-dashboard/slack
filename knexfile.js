f29902d4838cb8d03666356b93e89affaf33217d
Dave <wm.david.gallup@gmail.com>
2017-08-11 15:41:33 -0700

    Alter knexfile for AWS deploy

--- knexfile.js
+++ knexfile.js
@@ -24,6 +24,13 @@ module.exports = {
 
   production: {
     client: 'pg',
-    connection: process.env.DATABASE_URL,
+    connection: {
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      user: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+    },
   },
 };
+
