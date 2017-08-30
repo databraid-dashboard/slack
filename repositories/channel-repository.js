8b51b00d4baa5ecf228de70d9c85e9007f12d093
Kurtis Houser <houserkurtis@gmail.com>
2017-08-29 17:06:15 -0700

    Refactor db repositories to reflect updated db struture

--- repositories/channel-repository.js
+++ repositories/channel-repository.js
@@ -2,7 +2,7 @@ const knex = require('../knex.js');
 const { camelizeKeys } = require('humps');
 
 function getChannels() {
-  return knex('channel_map')
+  return knex('channels')
     .orderBy('channel_name')
     .then(result => camelizeKeys(result));
 }
--- repositories/event-repository.js
+++ repositories/event-repository.js
@@ -2,10 +2,10 @@ const knex = require('../knex.js');
 
 function writeMessage(userId, text, rawTS, channelId) {
   if (userId && text && rawTS && channelId) {
-    return knex('message')
+    return knex('messages')
       .insert({
-        user_map_id: knex.raw(`(select id from user_map where user_id = '${userId}')`),
-        channel_map_id: knex.raw(`(select id from channel_map where channel_id = '${channelId}')`),
+        user_id: userId,
+        channel_id: channelId,
         raw_ts: rawTS,
         message_timestamp: new Date(Number.parseFloat(rawTS) * 1000),
         message: text,
