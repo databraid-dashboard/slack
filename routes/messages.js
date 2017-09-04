58cbe2a16ef4dc7bfee1b1a67d95294f77e76d67
TYLER LANGENBRUNNER <MISTERLANGO@GMAIL.COM>
2017-09-03 18:33:39 -0700

    Send user information with message data to frontend

--- routes/messages.js
+++ routes/messages.js
@@ -16,9 +16,26 @@ router.get('/', (req, res, next) => {
 
 router.get('/:channelName', (req, res, next) => {
   getMessagesByChannelName(req.params.channelName)
-    .then((messages) => {
-      res.status(200).send(messages);
-    })
+    .then(messages =>
+      const tailoredMessages = messages.map((msgObj) => {
+        console.log('......', msgObj);
+        const msg = {};
+        msg.messageId = msgObj.messageId;
+        msg.avatarImage = msgObj.image24;
+        msg.name = msgObj.realName;
+        msg.userName = msgObj.userName;
+        msg.text = msgObj.message;
+        msg.timestamp = msgObj.messageTimestamp;
+        msg.rawTimestamp = msgObj.rawTs;
+        msg.channelName = msgObj.channelName;
+        msg.statusEmoji = msgObj.statusEmoji;
+        return msg;
+      });
+
+      console.log('SENDING TO FRONTEND >>>>>>', tailoredMessages);
+      return res.status(200).send(tailoredMessages);
+      // res.status(200).send(messages),
+    )
     .catch((err) => {
       next(err);
     });
